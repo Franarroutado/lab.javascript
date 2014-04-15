76996095250b7df6b98040c0233dc60a1f3294f7
Francisco Trigo <francisco.trigo@gmail.com>
2014-04-15 21:23:52 +0200

    cambios

--- currying.js
+++ currying.js
@@ -6,3 +6,17 @@ var babyAnimals = function(a) {
         return result;
     };
 };
+
+var sum = function(otherFunc) {
+
+    return function(a, b) {
+        var sumRec = function(a, b) {
+            if ( a > b ) 
+                return 0; 
+            else 
+                return otherFunc(a) + sumRec( a + 1, b);
+        };
+        return sumRec(a,b);
+    };
+    
+};
--- tests.js
+++ tests.js
@@ -10,3 +10,16 @@ test("Currying function", function() {
     var babyKoala = babyAnimals('koalas');
     ok(babyKoala('elephants') == 'i love koalas and elephants', 'It went wrong')
 });
+
+test("Currying function", function() {
+    var sumInts = sum(function(x) {
+        return x;
+    });
+    
+    var sumCubes = sum(function(x) {
+        return x * x * x;
+    });
+    console.log(sumInts);
+    ok(sumInts(1, 30) == 465, "Ha fallado la suma de Integers: " + sumInts(1, 30))
+    ok(sumCubes(1,3) == 36, "Ha fallado la suba de cubos usando currying: sumCubes(1,3): " + sumCubes(1,3) + " != " + 36)
+});
